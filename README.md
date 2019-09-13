795f6c1181aa5955fe29fa2e028ff1b2aecc3ddf
NewView <40295938+ClnViewer@users.noreply.github.com>
2019-09-13 07:28:00 +0300

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# C-printable-enumerator
+# C++ printable enumerator template
 C++17 enum printable template, one header source
+
+````
+ class eTest
+ {
+    public:
+     ENUM_PRINTABLE(EnumeratorName, int32_t,
+         ENUM_TAG_OKAY,  // == 0
+         ENUM_TAG_FAIL,
+         ENUM_TAG_DONE,
+         ENUM_TAG_SEND,
+         ENUM_TAG_RECV,
+         ENUM_TAG_DATA
+     );
+ };
+
+ ...
+  
+     // Get enum size by labels:
+     std::size_t s = eTest::sizeEnumeratorName();
+
+     // Get string by number:
+     std::cout << eTest::getEnumeratorName(2);
+
+     // Get number by enum value:
+     int32_t n = enumtoint(EnumeratorName::ENUM_TAG_DONE);
+  
+     // Get string by enum value:
+     std::cout << enumtostring(EnumeratorName::ENUM_TAG_DONE);
+  
+     // Get string by enum value to std::ostream:
+     std::cout << EnumeratorName::ENUM_TAG_DONE;
+
+````
+
---
+++ example.cpp
@@ -0,0 +1,35 @@
+
+#include <string>
+#include <iostream>
+
+/// C++17 Enumerator printable, header only
+#define _BUILD_ENUM_PRINTABLE 1
+#include "extEnumPrintable.h"
+
+class eTest
+{
+    public:
+
+        ENUM_PRINTABLE(EnumeratorName, int32_t,
+                ENUM_TAG_OKAY,  // == 0
+                ENUM_TAG_FAIL,
+                ENUM_TAG_DONE,
+                ENUM_TAG_SEND,
+                ENUM_TAG_RECV,
+                ENUM_TAG_DATA
+        );
+};
+
+int main()
+{
+    int32_t n = enumtoint(eTest::EnumeratorName::ENUM_TAG_DONE);
+    std::cout << "Test int     : " << n << std::endl;
+    std::cout << "Test string  : " << enumtostring(eTest::EnumeratorName::ENUM_TAG_DONE) << std::endl;
+    std::cout << "Test ostream : " << eTest::EnumeratorName::ENUM_TAG_DONE << std::endl;
+    std::cout << "Test from int: " << eTest::getEnumeratorName(2) << std::endl;
+
+    for (std::size_t i = 0U; i < eTest::sizeEnumeratorName(); i++)
+        std::cout << "\t- " << i << " : " << eTest::getEnumeratorName(i) << std::endl;
+
+    return 0;
+}
